bc79806197af2fb4771b9c1c8dceec7ba9af5cf4
John P. Masseria <jpmasseria@gmail.com>
2019-05-30 16:16:26 -0400

    Add code to output TCP input data (command) to serial tty

--- BlueLightTCP2Serial.py
+++ BlueLightTCP2Serial.py
@@ -1,6 +1,45 @@
 import socket
 import sys
 
+import serial, time
+
+SERIALPORT = "/dev/ttyUSB0"
+BAUDRATE = 19200
+
+ser = serial.Serial(SERIALPORT, BAUDRATE)
+ser.bytesize = serial.EIGHTBITS     # number of bits per bytes
+ser.parity = serial.PARITY_NONE     # set parity check: no parity
+ser.stopbits = serial.STOPBITS_ONE  # number of stop bits
+#ser.timeout = None                 # block read
+#ser.timeout = 0                    # non-block read
+ser.timeout = 2                     # timeout block read
+ser.xonxoff = False                 # disable software flow control
+ser.rtscts = False                  # disable hardware (RTS/CTS) flow control
+ser.dsrdtr = False                  # disable hardware (DSR/DTR) flow control
+ser.writeTimeout = 0                # timeout for write
+
+print >>sys.stderr, 'Starting Up Serial Monitor'
+
+try:
+    ser.open()
+
+except Exception, e:
+    print >>sys.stderr, "error open serial port: " + str(e)
+    exit(1)
+
+if ser.isOpen():
+
+    try:
+        ser.flushInput()            # flush input buffer, discarding all its contents
+        ser.flushOutput()           # flush output buffer, aborting current output
+
+    except Exception, e:
+        print >>sys.stderr, "error communicating...: " + str(e)
+
+else:
+    print >>sys.stderr, "cannot open serial port "
+    exit(1)
+
 CR = '\r'
 LF = '\n'
 
@@ -50,6 +89,7 @@ while True:
                     
                         if char == CR:
                             print >>sys.stderr, 'Command received "%s"' % ':'.join('{:02x}'.format(ord(c)) for c in input)
+                            ser.write(input)
                             input = ''
 
             else:
