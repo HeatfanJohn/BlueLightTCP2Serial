a7b810a76b0f9c376147c33fa7c7c0de545e1166
John P. Masseria <jpmasseria@gmail.com>
2019-06-06 15:13:47 -0400

    Complete code refactoring to eliminate all pylint warnings

--- blue_light_simulator.py
+++ blue_light_simulator.py
@@ -13,26 +13,29 @@ ON = "On"
 CR = '\r'
 LF = '\n'
 
-light_state = [OFF, OFF, OFF]
-
-def simulate_serial_response(ser, input):
-    if len(input) == 9:
-        if input[6].isdigit() and int(input[6]) <= len(light_state)-1:
-            if input[7] == '?':
-                ser.write(LF + 'Complete' + CR + LF + \
-                    'Plug ' + input[6] + ' ' + light_state[int(input[6])] + CR)
+def simulate_serial_response(in_serial, in_input, in_light_state):
+    """ Simulate a BlackBox Pow-R-Switch IR power switching device
+    by analyzing input and return correct simulated respone back out the
+    USB serial port.
+    """
+    if len(in_input) == 9:
+        if in_input[6].isdigit() and int(in_input[6]) <= len(in_light_state)-1:
+            if in_input[7] == '?':
+                in_serial.write(LF + 'Complete' + CR + LF + \
+                    'Plug ' + in_input[6] + ' ' + in_light_state[int(in_input[6])] + CR)
                 return
 
-            elif input[7].isdigit():
-                if input[7] == '0':
-                    light_state[int(input[6])] = OFF
-                elif input[7] == '1':
-                    light_state[int(input[6])] = ON
+            elif in_input[7].isdigit():
+                if in_input[7] == '0':
+                    in_light_state[int(in_input[6])] = OFF
+                elif in_input[7] == '1':
+                    in_light_state[int(in_input[6])] = ON
                 else:
-                    print >>sys.stderr, 'Invalid on/off state: "' + input + '"'
+                    print >>sys.stderr, 'Invalid on/off state: "' + in_input + '"'
                     return
 
-                print >>sys.stderr, 'Light ' + input[6] + " turned " + light_state[int(input[6])]
+                print >>sys.stderr, 'Light ' + in_input[6] \
+                    + " turned " + in_light_state[int(in_input[6])]
                 return
             else:
                 print >>sys.stderr, '8th character is not a digit or question mark'
@@ -41,50 +44,60 @@ def simulate_serial_response(ser, input):
     else:
         print >>sys.stderr, 'Input is not 9 characters'
 
-    print >>sys.stderr, 'Invalid input message: "' + input + '"'
+    print >>sys.stderr, 'Invalid input message: "' + in_input + '"'
 
-ser = serial.Serial(SERIALPORT, BAUDRATE)
-ser.bytesize = serial.EIGHTBITS     # number of bits per bytes
-ser.parity = serial.PARITY_NONE     # set parity check: no parity
-ser.stopbits = serial.STOPBITS_ONE  # number of stop bits
-#ser.timeout = None                 # block read
-#ser.timeout = 0                    # non-block read
-ser.timeout = 0.2                   # timeout block read in seconds
-ser.xonxoff = False                 # disable software flow control
-ser.rtscts = False                  # disable hardware (RTS/CTS) flow control
-ser.dsrdtr = False                  # disable hardware (DSR/DTR) flow control
-ser.writeTimeout = None             # timeout for write - None => non-blocking
+def blue_light_tcp_2_serial():
+    """ Read input from a USB serial port and simulate a BlackBox Pow-R-Switch IR
+    power switching device and return any simulated respone back out the same
+    USB serial port.
+    """
+    ser = serial.Serial(SERIALPORT, BAUDRATE)
+    ser.bytesize = serial.EIGHTBITS     # number of bits per bytes
+    ser.parity = serial.PARITY_NONE     # set parity check: no parity
+    ser.stopbits = serial.STOPBITS_ONE  # number of stop bits
+    #ser.timeout = None                 # block read
+    #ser.timeout = 0                    # non-block read
+    ser.timeout = 0.2                   # timeout block read in seconds
+    ser.xonxoff = False                 # disable software flow control
+    ser.rtscts = False                  # disable hardware (RTS/CTS) flow control
+    ser.dsrdtr = False                  # disable hardware (DSR/DTR) flow control
+    ser.writeTimeout = None             # timeout for write - None => non-blocking
 
-print >>sys.stderr, 'Starting up Blue Light Simulator'
+    light_state = [OFF, OFF, OFF]
 
-if ser.isOpen() is False:
-    ser.open()
+    print >>sys.stderr, 'Starting up Blue Light Simulator'
 
-if ser.isOpen():
-    ser.flushInput()            # flush input buffer, discarding all its contents
-    ser.flushOutput()           # flush output buffer, aborting current output
+    if ser.isOpen() is False:
+        ser.open()
 
-else:
-    print >>sys.stderr, "cannot open serial port "
-    exit(1)
+    if ser.isOpen():
+        ser.flushInput()            # flush input buffer, discarding all its contents
+        ser.flushOutput()           # flush output buffer, aborting current output
 
-input = ''
-while True:
-    # Wait for input - Timeout is set to 0.2 seconds
+    else:
+        print >>sys.stderr, "cannot open serial port "
+        exit(1)
 
-    data = ser.read(128)                # Read up to 128 bytes
+    input_so_far = ''
+    while True:
+        # Wait for input - Timeout is set to 0.2 seconds
 
-    if data:
-        print >>sys.stderr, 'received "%s"' % ':'.join('{:02x}'.format(ord(c)) for c in data)
-        for char in data:
-            if char == LF:              # Ignore line feed characters
-                continue
+        data = ser.read(128)                # Read up to 128 bytes
 
-            else:
-                input = input + char    # Append this character onto input
+        if data:
+            print >>sys.stderr, 'received "%s"' % ':'.join('{:02x}'.format(ord(c)) for c in data)
+            for char in data:
+                if char == LF:              # Ignore line feed characters
+                    continue
 
-                if char == CR:          # Send input out serial port
-                    print >>sys.stderr, 'Command received "%s"' % ':' \
-                        .join('{:02x}'.format(ord(c)) for c in input)
-                    simulate_serial_response(ser, input)
-                    input = ''          # Reset input buffer
+                else:
+                    input_so_far = input_so_far + char    # Append this character onto input
+
+                    if char == CR:          # Send input out serial port
+                        print >>sys.stderr, 'Command received "%s"' % ':' \
+                            .join('{:02x}'.format(ord(c)) for c in input_so_far)
+                        simulate_serial_response(ser, input_so_far, light_state)
+                        input_so_far = ''          # Reset input buffer
+
+if __name__ == '__main__':
+    blue_light_tcp_2_serial()
