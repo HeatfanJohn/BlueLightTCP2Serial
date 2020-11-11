a1347d0b29494df05f67d26182905dbdabe135f6
John P. Masseria <jpmasseria@gmail.com>
2020-11-10 22:14:55 -0500

    Update tests to wait 1 sec to allow response to be returned to nc

--- tests/Light-0-off.sh
+++ tests/Light-0-off.sh
@@ -1,1 +1,2 @@
-nc -N localhost 1000 < ./Light-0-off.hex
+#nc -N localhost 1000 < ./Light-0-off.hex
+{ cat 0-off.hex; sleep 1; } | nc -N localhost 1000
--- tests/Light-0-on.sh
+++ tests/Light-0-on.sh
@@ -1,1 +1,2 @@
-nc -N localhost 1000 < ./Light-0-on.hex
+#nc -N localhost 1000 -q 3 < ./0-on.hex
+{ cat 0-on.hex; sleep 1; } | nc -N localhost 1000
